a110f92ef40d8e4c1c48c34f5bcdf1e496eb711a
paigemoody <moodypaige.e@gmail.com>
2025-03-31 16:25:09 -0400

    remove hardcoding

--- visual_scout/constants.py
+++ visual_scout/constants.py
@@ -8,27 +8,29 @@ COST_PER_MILLION_TOKENS_4o_OUTPUT = 10
 COST_PER_MILLION_TOKENS_4o_mini_INPUT = 0.150
 COST_PER_MILLION_TOKENS_4o_mini_OUTPUT = 0.600
 
+# ------ Token count estimates ------ #
+
+# -- Based on rough estimates for input and output tokens:
+# ~415 tokens in base promt
+# ~1215 tokens in grid content
+# ~35 output labels per grid -> ~300 tokens
+# details in: https://github.com/paigemoody/visual-scout/issues/7#issuecomment-2724828185
+# --
+EST_INPUT_TOKEN_COUNT_PER_REQUEST = 1700
+EST_OUTPUT_TOKEN_COUNT_PER_REQUEST = 300
+
 # ------------ #
 # DO NOT EDIT BELOW THIS LINE
 # ------------ #
 
 # Get cost per token
 # 4o
-COST_PER_TOKEN_4o_INPUT = 2.50 / 1000000
-COST_PER_TOKEN_4o_OUTPUT = 10 / 1000000
+COST_PER_TOKEN_4o_INPUT = COST_PER_MILLION_TOKENS_4o_INPUT / 1000000
+COST_PER_TOKEN_4o_OUTPUT = COST_PER_MILLION_TOKENS_4o_OUTPUT / 1000000
 
 # 4o-mini
-COST_PER_TOKEN_4o_mini_INPUT = 0.150 / 1000000
-COST_PER_TOKEN_4o_mini_OUTPUT = 0.600 / 1000000
-
-# ------ Token counts from estimates ------ #
-
-# --
-# ~415 tokens in base promt, ~1215 tokens in grid content, 35 output labels per grid
-# details in: https://github.com/paigemoody/visual-scout/issues/7#issuecomment-2724828185
-# --
-EST_INPUT_TOKEN_COUNT_PER_REQUEST = 1700
-EST_OUTPUT_TOKEN_COUNT_PER_REQUEST = 300
+COST_PER_TOKEN_4o_mini_INPUT = COST_PER_MILLION_TOKENS_4o_mini_INPUT / 1000000
+COST_PER_TOKEN_4o_mini_OUTPUT = COST_PER_MILLION_TOKENS_4o_mini_OUTPUT / 1000000
 
 # ------ MATH ------ #
 
