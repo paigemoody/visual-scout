f7be0d0a07c1e50986e0e8f0206f9a373eb53e32
paigemoody <moodypaige.e@gmail.com>
2025-03-31 14:27:42 -0400

    streamline setuptools

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+setuptools.setup(
     name="visual-scout",
     version="0.1.0",
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     entry_points={
         "console_scripts": [
             "visual-scout=visual_scout.cli:main",
